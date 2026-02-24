8bce0566ae19eddbfb62c137f44060612795c7ac
Koushik Barman <bkoushik925@gmail.com>
2026-02-24 19:07:30 +0530

    Update branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,24 @@
 import { Routes } from '@angular/router';
-import { Ragisterpage } from './registerpage/registerpage';
+import { Registerpage } from './registerpage/registerpage';
 import { Loginpage } from './loginpage/loginpage';
 import { Homepage } from './homepage/homepage';
-import { StudentDashboard } from './student-dashboard/student-dashboard';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard';
 import { AdminDashboard } from './admin-dashboard/admin-dashboard';
 import { SuperAdminDashboard } from './super-admin-dashboard/super-admin-dashboard';
 import { SignupSuccessComponent } from './signup-success/signup-success.component';
 import { roleGuard } from './role/role';
 
-export const routes: Routes = [
 
+export const routes: Routes = [
   { path: '', component: Homepage },
 
-  { path: 'register', component: Ragisterpage },
+  { path: 'register', component: Registerpage },
   { path: 'login', component: Loginpage },
   { path: 'signup-success', component: SignupSuccessComponent },
 
   {
     path: 'student-dashboard',
-    component: StudentDashboard,
+    component: StudentDashboardComponent,
     canActivate: [roleGuard('student')]
   },
 
@@ -34,3 +34,4 @@ export const routes: Routes = [
     canActivate: [roleGuard('super_admin')]
   }
 ];
+
